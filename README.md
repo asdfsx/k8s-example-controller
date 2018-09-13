a1d43abc732dcc94a18e4a9e39191d3f9e7a0055
sam.sun <asdfsx@gmail.com>
2018-09-13 14:20:36 +0800

    add source code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # k8s-example-controller
+
+according to project: https://github.com/trstringer/k8s-controller-core-resource
---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	api_v1 "k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/workqueue"
+
+	"github.com/asdfsx/k8s-example-controller/pkg/signals"
+)
+
+var (
+	masterURL  string
+	kubeconfig string
+)
+
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+}
+
+func main() {
+	flag.Parse()
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		log.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	client, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		log.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	// create the informer so that we can not only list resources
+	// but also watch them for all pods in the default namespace
+	informer := cache.NewSharedIndexInformer(
+		// the ListWatch contains two different functions that our
+		// informer requires: ListFunc to take care of listing and watching
+		// the resources we want to handle
+		&cache.ListWatch{
+			ListFunc: func(options meta_v1.ListOptions) (runtime.Object, error) {
+				// list all of the pods (core resource) in the deafult namespace
+				fmt.Println("-------------------------Listfunc")
+				return client.CoreV1().Pods(meta_v1.NamespaceDefault).List(options)
+			},
+			WatchFunc: func(options meta_v1.ListOptions) (watch.Interface, error) {
+				// watch all of the pods (core resource) in the default namespace
+				fmt.Println("-------------------------WatchFunc")
+				return client.CoreV1().Pods(meta_v1.NamespaceDefault).Watch(options)
+			},
+		},
+		&api_v1.Pod{}, // the target type (Pod)
+		0,             // no resync (period of 0)
+		cache.Indexers{},
+	)
+
+	// create a new queue so that when the informer gets a resource that is either
+	// a result of listing or watching, we can add an idenfitying key to the queue
+	// so that it can be handled in the handler
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	// add event handlers to handle the three types of events for resources:
+	//  - adding new resources
+	//  - updating existing resources
+	//  - deleting resources
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			// convert the resource object into a key (in this case
+			// we are just doing it in the format of 'namespace/name')
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			log.Infof("Add pod: %s", key)
+			if err == nil {
+				// add the key to the queue for the handler to get
+				queue.Add(key)
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(newObj)
+			log.Infof("Update pod: %s", key)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			// DeletionHandlingMetaNamsespaceKeyFunc is a helper function that allows
+			// us to check the DeletedFinalStateUnknown existence in the event that
+			// a resource was deleted but it is still contained in the index
+			//
+			// this then in turn calls MetaNamespaceKeyFunc
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			log.Infof("Delete pod: %s", key)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+	})
+
+	// construct the Controller object which has all of the necessary components to
+	// handle logging, connections, informing (listing and watching), the queue,
+	// and the handler
+	controller := Controller{
+		logger:    log.NewEntry(log.New()),
+		clientset: client,
+		informer:  informer,
+		queue:     queue,
+		handler:   &TestHandler{},
+	}
+
+	// run the controller loop to process items
+	go controller.Run(stopCh)
+
+	log.Info("Started workers")
+	<-stopCh
+	log.Info("Shutting down workers")
+}
